3692b4ff7f6164da14a4d8e14f7aef101708e4b0
RufusFurorem <williamdbuchanan1@gmail.com>
2020-12-10 18:03:41 -0500

    Add print for three sum

--- day_1/report_repair_two_and_three_sum.py
+++ day_1/report_repair_two_and_three_sum.py
@@ -6,6 +6,18 @@ def two_sum(expense_report, target):
             return (val * compl_dict[val])
         compl_dict[compl] = val
 
+def three_sum(expense_report, target):
+    compl_dict = {}
+    two_vals = []
+    for i, val in enumerate(expense_report):
+        two_vals.append([(val, num) for j, num in enumerate(expense_report) if j != i])
+
+    for sums in two_vals:
+        for value in sums:
+            for exp_val in expense_report:
+                if (value[0] + value[1] + exp_val) == target:
+                    print(value[0], value[1], exp_val)
+                    return (value[0] * value[1] * exp_val)
 
 if __name__ == "__main__":
     with open('..\\input.csv', 'r') as fd:
@@ -14,3 +26,4 @@ if __name__ == "__main__":
         expense_values = list(map(int, expense_values))
     target_value = 2020
     print(two_sum(expense_values, target_value))
+    print(three_sum(expense_values, target_value))
