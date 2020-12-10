f1cf393fed6d777202ad13e4800db642ea8b023b
RufusFurorem <williamdbuchanan1@gmail.com>
2020-12-10 17:20:43 -0500

    Move days into directory and fix input path

--- day_1/two_sum_aoc.py
+++ day_1/two_sum_aoc.py
@@ -2,15 +2,26 @@
 def two_sum(expense_report, target):
     compl_dict = {}
     for val in expense_report:
+        compl = target - val
+        if val in compl_dict:
+            return (val * compl_dict[val])
+        compl_dict[compl] = val
+
+
+def three_sum(expense_report, target):
+    compl_dict = {}
+    for val in expense_report:
         val = int(val)
         compl = target - val
         if val in compl_dict:
             return (val * compl_dict[val])
         compl_dict[compl] = val
 
+
 if __name__ == "__main__":
-    with open('input.csv', 'r') as fd:
+    with open('..\\input.csv', 'r') as fd:
         lines = fd.read().strip()
         expense_values = lines.split('\n')
+        expense_values = list(map(int, expense_values))
     target_value = 2020
     print(two_sum(expense_values, target_value))
